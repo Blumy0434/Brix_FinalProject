b2ee834b443fb58f8ad8bd4eb10ef8d18178457a
M.B <mb0583219718@gmail.com>
2020-07-26 13:34:10 +0300

    account handler

---
+++ Account.NSB.Services/Interfaces/IAccountNSBService.cs
@@ -0,0 +1,10 @@
+﻿using Messages.Commands;
+using System.Threading.Tasks;
+
+namespace Account.NSB.Service.Interfaces
+{
+    public interface IAccountNSBService
+    {
+        Task<bool> UpdateAccounts(UpdateAccounts message);
+    }
+}
